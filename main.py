318b9de6ad19961f2c61217672037ae857024e5c
Tumin <contact.tumins@gmail.com>
2026-06-25 17:49:35 +0530

    feat: mujoco python script for test

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from 5-bar!")
+import mujoco
+import mujoco.viewer
+import numpy as np
 
+model = mujoco.MjModel.from_xml_path("five_bar.xml")
+data = mujoco.MjData(model)
+
+servo_L = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, "servo_L")
+servo_R = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, "servo_R")
+
+def main(n):
+    with mujoco.viewer.launch_passive(model, data) as viewer:
+        while viewer.is_running():
+            target_pos = np.sin(data.time/50)
+
+            data.ctrl[servo_L] = target_pos
+            data.ctrl[servo_R] = -1 * target_pos
+
+            mujoco.mj_step(model, data)
+            viewer.sync()
 
 if __name__ == "__main__":
-    main()
+    main(5.0)
